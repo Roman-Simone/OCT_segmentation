091454eb7fb5af5394bbeaf31a876088c7211682
Roman Simone <simone.roman.2001@gmail.com>
2025-03-19 11:02:39 +0100

    Save current work

---
+++ src/evaluation/evaluation.py
@@ -0,0 +1,128 @@
+import multiprocessing
+import os
+import json
+from copy import deepcopy
+from multiprocessing import Pool
+from typing import Tuple, List, Union, Optional
+
+import numpy as np
+import SimpleITK as sitk
+
+class Evaluation:
+    
+    def __init__(self, folder_ref: str, folder_pred: str, output_file: str, file_ending: str, regions: List[int], ignore_label: int = None, num_processes: int = 8):
+        self.folder_ref = folder_ref
+        self.folder_pred = folder_pred
+        self.output_file = output_file
+        self.file_ending = file_ending
+        self.regions = regions
+        self.ignore_label = ignore_label
+        self.num_processes = num_processes
+    
+
+    def label_or_region_to_key(self, label_or_region: Union[int, Tuple[int]]):
+        return str(label_or_region)
+
+    def key_to_label_or_region(self, key: str):
+        try:
+            return int(key)
+        except ValueError:
+            key = key.replace('(', '').replace(')', '')
+            split = key.split(',')
+            return tuple([int(i) for i in split if len(i) > 0])
+
+    def save_json(self, data: dict, filename: str, sort_keys: bool = True):
+        def convert_types(obj):
+            if isinstance(obj, np.integer):
+                return int(obj)
+            elif isinstance(obj, np.floating):
+                return float(obj)
+            elif isinstance(obj, dict):
+                return {k: convert_types(v) for k, v in obj.items()}
+            elif isinstance(obj, list):
+                return [convert_types(v) for v in obj]
+            return obj
+        
+        data = convert_types(data)
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=4, sort_keys=sort_keys)
+            
+    
+    def load_json(self, filename: str):
+        with open(filename, 'r') as f:
+            return json.load(f)
+
+    def labels_to_list_of_regions(self, labels: List[int]):
+        return [(i,) for i in labels]
+
+    def region_or_label_to_mask(self,segmentation: np.ndarray, region_or_label: Union[int, Tuple[int, ...]]) -> np.ndarray:
+        if np.isscalar(region_or_label):
+            return segmentation == region_or_label
+        else:
+            mask = np.zeros_like(segmentation, dtype=bool)
+            for r in region_or_label:
+                mask[segmentation == r] = True
+        return mask
+
+    def compute_tp_fp_fn_tn(self, mask_ref: np.ndarray, mask_pred: np.ndarray, ignore_mask: np.ndarray = None):
+        if ignore_mask is None:
+            use_mask = np.ones_like(mask_ref, dtype=bool)
+        else:
+            use_mask = ~ignore_mask
+        tp = np.sum((mask_ref & mask_pred) & use_mask)
+        fp = np.sum(((~mask_ref) & mask_pred) & use_mask)
+        fn = np.sum((mask_ref & (~mask_pred)) & use_mask)
+        tn = np.sum(((~mask_ref) & (~mask_pred)) & use_mask)
+        return tp, fp, fn, tn
+
+    def compute_metrics(self, reference_file: str, prediction_file: str, labels_or_regions: Union[List[int], List[Union[int, Tuple[int, ...]]]], ignore_label: int = None) -> dict:
+        seg_ref = sitk.GetArrayFromImage(sitk.ReadImage(reference_file))
+        seg_pred = sitk.GetArrayFromImage(sitk.ReadImage(prediction_file))
+        ignore_mask = seg_ref == ignore_label if ignore_label is not None else None
+        
+        results = {'reference_file': reference_file, 'prediction_file': prediction_file, 'metrics': {}}
+        for r in labels_or_regions:
+            key = self.label_or_region_to_key(r)
+            results['metrics'][key] = {}
+            mask_ref = self.region_or_label_to_mask(seg_ref, r)
+            mask_pred = self.region_or_label_to_mask(seg_pred, r)
+            tp, fp, fn, tn = self.compute_tp_fp_fn_tn(mask_ref, mask_pred, ignore_mask)
+            results['metrics'][key]['Dice'] = 2 * tp / (2 * tp + fp + fn) if tp + fp + fn > 0 else np.nan
+            results['metrics'][key]['IoU'] = tp / (tp + fp + fn) if tp + fp + fn > 0 else np.nan
+            results['metrics'][key].update({'FP': fp, 'TP': tp, 'FN': fn, 'TN': tn, 'n_pred': fp + tp, 'n_ref': fn + tp})
+            results['metrics'][key]['f1'] = 2 * tp / (2 * tp + fp + fn) if tp + fp + fn > 0 else np.nan 
+        return results
+
+    def compute_metrics_on_folder(self, folder_ref: str, folder_pred: str, output_file: str, file_ending: str, regions_or_labels: Union[List[int], List[Union[int, Tuple[int, ...]]]], ignore_label: int = None, num_processes: int = 8, chill: bool = False) -> dict:
+        files_pred = [f for f in os.listdir(folder_pred) if f.endswith(file_ending)]
+        files_ref = [f for f in os.listdir(folder_ref) if f.endswith(file_ending)]
+        if not chill:
+            assert all(os.path.isfile(os.path.join(folder_pred, f)) for f in files_ref), "Not all files in folder_ref exist in folder_pred"
+        files_ref = [os.path.join(folder_ref, f) for f in files_pred]
+        files_pred = [os.path.join(folder_pred, f) for f in files_pred]
+        
+        with multiprocessing.get_context("spawn").Pool(num_processes) as pool:
+            results = pool.starmap(self.compute_metrics, zip(files_ref, files_pred, [regions_or_labels] * len(files_pred), [ignore_label] * len(files_pred)))
+        
+        metric_list = list(results[0]['metrics'][self.label_or_region_to_key(regions_or_labels[0])].keys())
+        means = {self.label_or_region_to_key(r): {m: np.nanmean([i['metrics'][self.label_or_region_to_key(r)][m] for i in results]) for m in metric_list} for r in regions_or_labels}
+        foreground_mean = {m: np.mean([means[k][m] for k in means.keys() if k != '0']) for m in metric_list}
+        
+        result = {'metric_per_case': results, 'mean': means, 'foreground_mean': foreground_mean}
+        if output_file is not None:
+            self.save_json(result, output_file)
+        return result
+
+def main():
+    folder_ref = '/Users/simoneroman/Desktop/OCT_segmentation/data/RETOUCH_PROCESSED_NNUNET/nnUNet_raw/Dataset002_Set/labelsTr'
+    folder_pred = 'output2'
+    output_file = 'summary2.json'
+    file_ending = '.nii.gz'
+    regions = [0, 1, 2, 3]
+    ignore_label = None
+    num_processes = 8
+    eval = Evaluation(folder_ref, folder_pred, output_file, file_ending, regions, ignore_label, num_processes)
+    eval.compute_metrics_on_folder(folder_ref, folder_pred, output_file, file_ending, regions, ignore_label, num_processes)
+
+if __name__ == "__main__":
+    main()
---
+++ src/evaluation/read.py
@@ -0,0 +1,67 @@
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+
+# Percorsi ai file
+label_path = "data/RETOUCH_PROCESSED_NNUNET/nnUNet_raw/Dataset002_Set/labelsTr/TRAIN_049.nii.gz"
+image_path = "data/RETOUCH_PROCESSED_NNUNET/nnUNet_raw/Dataset002_Set/imagesTr/TRAIN_049_0000.nii.gz"
+prediction_path = "output2/TRAIN_049.nii.gz"
+
+# Carica i dati
+image = nib.load(image_path).get_fdata()
+label = nib.load(label_path).get_fdata() 
+prediction = nib.load(prediction_path).get_fdata()
+
+# Ottieni dimensioni
+num_slices = image.shape[2]
+slice_idx = num_slices // 2  # Slice iniziale (centrale)
+
+# Crea la figura e i subplot
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
+plt.subplots_adjust(bottom=0.2)
+
+# Mostra immagini iniziali con imshow (senza ridisegnare ogni volta)
+img1 = ax1.imshow(image[:, :, slice_idx], cmap="gray")
+ax1.set_title("Immagine Originale")
+ax1.axis("off")
+
+img2 = ax2.imshow(image[:, :, slice_idx], cmap="gray")
+overlay2 = ax2.imshow(label[:, :, slice_idx], cmap="jet", alpha=0.5)
+ax2.set_title("Ground Truth (Label)")
+ax2.axis("off")
+
+img3 = ax3.imshow(image[:, :, slice_idx], cmap="gray")
+overlay3 = ax3.imshow(prediction[:, :, slice_idx], cmap="jet", alpha=0.5)
+ax3.set_title("Segmentazione Predetta")
+ax3.axis("off")
+
+# Funzione per aggiornare le immagini senza ridisegnare tutto
+def update(slice_idx):
+    slice_idx = int(slice_idx)  # Converti in intero
+    img1.set_data(image[:, :, slice_idx])
+    img2.set_data(image[:, :, slice_idx])
+    overlay2.set_data(label[:, :, slice_idx])
+    img3.set_data(image[:, :, slice_idx])
+    overlay3.set_data(prediction[:, :, slice_idx])
+    fig.canvas.draw_idle()  # Aggiorna la figura
+
+# Callback per la tastiera
+def on_key(event):
+    global slice_idx
+    if event.key == "right":
+        slice_idx = min(slice_idx + 1, num_slices - 1)
+    elif event.key == "left":
+        slice_idx = max(slice_idx - 1, 0)
+    update(slice_idx)
+    slider.set_val(slice_idx)  # Aggiorna anche lo slider
+
+# Aggiungi lo slider per scorrere le slice
+ax_slider = plt.axes([0.2, 0.05, 0.6, 0.03], facecolor="lightgray")
+slider = Slider(ax_slider, "Slice", 0, num_slices - 1, valinit=slice_idx, valstep=1)
+slider.on_changed(lambda val: update(int(val)))
+
+# Assegna la funzione alla pressione dei tasti
+fig.canvas.mpl_connect("key_press_event", on_key)
+
+plt.show()
