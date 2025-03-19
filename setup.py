da09b7c9c9262527a53eb9e9feea41d1ecdca285
Roman Simone <simone.roman.2001@gmail.com>
2025-03-19 11:39:35 +0100

    Save current work

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
-import setuptools
+import subprocess
+from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setuptools.setup()
+# Function to install the submodule in editable mode
+def install_nnUNet():
+    # Path to the submodule
+    submodule_path = 'src/models/nnUNet'
+    
+    # Run pip install -e inside the submodule
+    subprocess.check_call(['pip', 'install', '-e', submodule_path])
+
+# Run the installation of nnUNet before setup
+install_nnUNet()
+
+# Standard setup configuration
+setup(
+    name='oct_segmentation',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add your project dependencies here
+    ],
+    # Other setup arguments as needed
+)
